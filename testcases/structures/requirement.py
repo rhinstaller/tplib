c9ecdd212c553cc309d7fa1f938d878e631ccc19
Pavel Holica <pholica@redhat.com>
2020-01-22 12:10:38 +0100

    Requirement data structure.

---
+++ testcases/structures/requirement.py
@@ -0,0 +1,31 @@
+import functools
+from . import Mapping as m
+from .data_object import DataObject, DocumentObject, ListObject
+
+class VerifiedBy(DataObject):
+    mapping = dict((
+        m('direct_list', required=False, func=list),
+        m('query', required=False),
+    ))
+
+
+class AcceptanceCriteria(DataObject):
+    mapping = dict((
+        m('direct_list', required=False, func=list),
+        m('query', required=False),
+        m('named_query', required=False),
+    ))
+
+
+class Requirement(DocumentObject):
+    mapping = dict((
+        m('name'),
+        m('description'),
+        m('tags', required=False, default=[], func=set),
+        m('verified_by', func=VerifiedBy),
+        m('acceptance_criteria', required=False, func=dict),
+    ))
+
+    @property
+    def id(self):
+        return self.name
