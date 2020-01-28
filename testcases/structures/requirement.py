a40d611b23a91529e0e018cd3b2802f78c235878
Pavel Holica <pholica@redhat.com>
2020-01-28 10:29:36 +0100

    Process direct_list for verified_by in Requirement.

--- testcases/structures/requirement.py
+++ testcases/structures/requirement.py
@@ -33,3 +33,34 @@ class Requirement(DocumentObject):
     @property
     def id(self):
         return self.name
+
+    def stabilize(self):
+        for query_type, query in self.verified_by.data.items():
+            if query is None:
+                continue
+            for testcase in self._findVerificationCases(query_type, query):
+                testcase.verifiesRequirement.add(self)
+                self.verificationTestCases.add(testcase)
+        return True
+
+    def _directListCases(self, cases_list):
+        return set(
+            [ self.library.testcases[case_name] for case_name in cases_list ]
+        )
+
+    def _directQueryCases(self, query):
+        raise Exception('NOT IMPLEMENTED')
+
+    def _namedQueryCases(self, query_name):
+        raise Exception('NOT IMPLEMENTED')
+
+    def _findVerificationCases(self, query_type, query):
+        methods = {
+            'direct_list' : self._directListCases,
+            'query' : self._directQueryCases,
+            'named_query' : self._namedQueryCases,
+        }
+        try:
+            return methods[query_type](query)
+        except KeyError:
+            raise ValueError("Unknown query type: '%s'" % query_type)
