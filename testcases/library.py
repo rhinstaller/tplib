dfd11e7eca4cf96a2ffda3247c0477d6338d1177
Pavel Holica <pholica@redhat.com>
2020-02-04 08:00:54 +0100

    Load library from single directory.
    Also change structure in tests directory so that the recursive loading
    is tested.

--- testcases/library.py
+++ testcases/library.py
@@ -5,12 +5,11 @@ import fnmatch
 from .structures.testcase import TestCase
 from .structures.requirement import Requirement
 
-def _iter_documents(directories, pattern):
-    for directory in directories:
-        for root, dirs, files in os.walk(directory):
-            for filename in files:
-                if fnmatch.fnmatchcase(filename, pattern):
-                    yield os.path.join(root, filename)
+def _iter_documents(directory, pattern):
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if fnmatch.fnmatchcase(filename, pattern):
+                yield os.path.join(root, filename)
 
 def diff(old, new):
     old_requirements = set(old.requirements)
@@ -55,21 +54,19 @@ def diff(old, new):
     return retval
 
 class Library():
-    def __init__(self, directories=None):
-        if directories is None:
-            directories = []
-        self.directories = directories
+    def __init__(self, directory):
+        self.directory = directory
         self.testcases = self._load_testcases()
         self.requirements = self._load_requirements()
         self._calculate_and_stabilize()
 
     def _load_testcases(self):
-        return self._load_structures(self.directories, '*.tc.yaml', TestCase)
+        return self._load_structures(self.directory, '*.tc.yaml', TestCase)
 
     def _load_requirements(self):
-        return self._load_structures(self.directories, '*.req.yaml', Requirement)
+        return self._load_structures(self.directory, '*.req.yaml', Requirement)
 
-    def _load_structures(self, directories, pattern, cls):
+    def _load_structures(self, directory, pattern, cls):
         structures = dict()
         for docfile in _iter_documents(directories, pattern):
             structure = cls(docfile, library=self)
