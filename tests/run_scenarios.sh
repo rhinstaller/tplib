ecd072432b5becc0d931485bc73ee0a238b15cfd
Pavel Holica <pholica@redhat.com>
2020-02-11 07:29:00 +0100

    Use uuid for testcaseid instead of plain names.
    Change-Id: I897719307bb653d0c868bc8330f1b3740a5913bf

--- tests/run_scenarios.sh
+++ tests/run_scenarios.sh
@@ -6,6 +6,18 @@ cleanup() {
     rm ${tmpfile}
 }
 
+assertRC() {
+    # usage: assertRC RC_CODE [command arg1 arg2 ... argN]
+    # exit if RC_CODE doesn't match return code of the command with arguments
+    local expected=${1}
+    shift
+    set +e
+    "${@}"
+    rc=$?
+    set -e
+    [[ ${rc} == ${expected} ]]
+}
+
 trap cleanup EXIT
 
 for scenario in scenarios/*; do
