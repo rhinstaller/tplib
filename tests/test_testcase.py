79ff8297c594f29f0fed71b0efbbbc78cbd8bcc5
Zdenek Veleba <zveleba@redhat.com>
2022-02-09 08:42:39 +0100

    Add basic unittests

---
+++ tests/test_testcase.py
@@ -0,0 +1,15 @@
+import unittest
+from tclib.structures import testcase
+
+class TestTestcase(unittest.TestCase):
+    def test_simple_load(self):
+        tc = testcase.TestCase('./data/baselib/ignition.tc.yaml')
+        expected = {'name': 'Ignition',
+                    'description': 'Tests ignition system',
+                    'priority': 6,
+                    'execution': {'type': 'manual'},
+                    'instructions': {'steps': [{'step': 'Connect', 'result': 'Connected'},
+                                               {'step': 'Try to start', 'result': 'Started'}]},
+                    'author': 'tester@example.com',
+                    'tags': ['electronics']}
+        self.assertEqual(tc.serialize(), expected)
