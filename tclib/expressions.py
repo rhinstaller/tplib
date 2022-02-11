52fd894957005ba5c383e8feacfd867526154854
Pavel Holica <pholica@redhat.com>
2022-02-11 10:39:56 +0100

    Add option to use compiled jinja2 templates

--- tclib/expressions.py
+++ tclib/expressions.py
@@ -1,15 +1,23 @@
 import jinja2
 import jinja2.nativetypes
 
-def eval(expression, **kwargs):
+def compile_native(expression):
     expression = "{{ %s }}" % expression
-    template = jinja2.nativetypes.NativeTemplate(expression)
-    return template.render(**kwargs)
+    return jinja2.nativetypes.NativeTemplate(expression).render
+
+def eval(expression, **kwargs):
+    return compile_native(expression)(**kwargs)
+
+def compile_str(expression):
+    expression = "{{ %s }}" % expression
+    return jinja2.Template(expression).render
 
 def eval_str(expression, **kwargs):
-    expression = "{{ %s }}" % expression
-    template = jinja2.Template(expression)
-    return template.render(**kwargs)
+    return compile_str(expression)(**kwargs)
+
+def compile_bool(expression):
+    render_func = compile_native(expression)
+    return lambda **kwargs: bool(render_func(**kwargs))
 
 def eval_bool(expression, **kwargs):
-    return bool(eval(expression, **kwargs))
+    return compile_bool(expression)(**kwargs)
