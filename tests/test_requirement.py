2a80b35e5801f9a3de4df9a3746b3468fea28d1b
Zdenek Veleba <zveleba@redhat.com>
2022-02-17 15:23:57 +0100

    Add unittests for requirement stabilization

--- tests/test_requirement.py
+++ tests/test_requirement.py
@@ -1,7 +1,8 @@
 import unittest
+from tclib import library
 from tclib.structures import requirement
 
-class TestRequirement(unittest.TestCase):
+class TestLoadRequirement(unittest.TestCase):
     def test_simple_load(self):
         req = requirement.Requirement('./data/baselib/running.req.yaml')
         expected = {'name': 'Running',
@@ -10,3 +11,21 @@ class TestRequirement(unittest.TestCase):
                                     'query': '"engine" in tc.tags'},
                     'acceptance_criteria': {}}
         self.assertEqual(req.serialize(), expected)
+
+class TestStabilizeRequirement(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.library_dir = './data/baselib'
+        cls.baselib = library.Library(cls.library_dir)
+
+    def test_verified_by_tc(self):
+        # Test case from direct_list
+        self.assertIn(self.baselib.testcases['Ignition'], self.baselib.requirements['Running'].verificationTestCases)
+        # Test case from query
+        self.assertIn(self.baselib.testcases['Ignition'], self.baselib.requirements['Electronics'].verificationTestCases)
+
+    def test_acceptance_criteria_tc(self):
+        # Test case from direct_list
+        self.assertIn(self.baselib.testcases['Steering wheel'], self.baselib.requirements['Controls'].acceptanceTestCases)
+        # Test case from query
+        self.assertIn(self.baselib.testcases['Ignition'], self.baselib.requirements['Electronics'].acceptanceTestCases)
